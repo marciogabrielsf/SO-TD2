640f37c0186db9f445a4e44778e3b9f49502e7e4
marciogabrielsf <marciogdsf@gmail.com>
2023-06-18 19:22:14 -0300

    Complete

--- backup_script.sh
+++ backup_script.sh
@@ -1,23 +1,53 @@
-pasta_origem=$1
-pasta_destino=$2
+pasta_origem=$1 #pasta de origem vinda do projeto2.sh
+pasta_destino=$2 #pasta de destino vinda do projeto2.sh
 
 backup_folder()
 {
+    #Verifica se a pasta de destino existe, se não existir, cria uma.
     if [ ! -d $pasta_destino ]; then
         echo "Pasta $pasta_destino não existe, criando uma..."
         mkdir $pasta_destino
         # exit 1
     fi
 
+    #Verifica se a pasta de origem existe, se não existir, cria uma.
     if [ ! -d $pasta_origem ]; then
         mkdir $pasta_origem
     fi
 
+    #Verifica se a pasta de destino está vazia, se não estiver, apaga tudo.
     if  ls -A1q $pasta_destino | grep -q . ; then
         rm -r $pasta_destino/*
     fi
 
-    cp -r $pasta_origem/* $pasta_destino
+    #Verifica se a pasta de origem está vazia, se não estiver, copia tudo.
+    if  ls -A1q $pasta_origem | grep -q . ; then
+        cp -r $pasta_origem/* $pasta_destino #copia tudo da pasta de origem para a pasta de destino
+    fi
+
 }
 
-backup_folder
+generate_log() 
+{ #função que gera o log
+
+    #Faz um grep no ausearch com a chave da pasta e filtra apenas os eventos do tipo SYSCALL
+    filtered_log="$(ausearch -i -k pasta | grep "SYSCALL")"
+
+    #Limpa o arquivo de log antes de escrever nele
+    echo > log.txt
+    
+    #Lê o que ele recebe do ausearch
+    echo "$filtered_log" | while IFS= read -r line; do #para cada linha do log, pegue o tipo, data e pid
+
+
+        type=$(echo "$line" | grep -oP '(?<=type=)[^[:space:]]+') #dá um grep na linha e filtra apenas o tipo.
+        date=$(echo "$line" | grep -oP '\d{2}/\d{2}/\d{4} \d{2}:\d{2}:\d{2}') #dá um grep na linha e filtra apenas a data.
+        pid=$(echo "$line" | grep -oP '(?<=\s)pid=[0-9]+' | cut -d= -f2) #dá um grep na linha e filtra apenas o pid.
+    
+    echo "Evento detectado: Date: $date | Type: $type | PID: $pid" >> log.txt #escreve no arquivo log o evento.
+    done
+}
+
+
+generate_log #chama a função de log
+backup_folder #chama a função de backup
--- projeto2.sh
+++ projeto2.sh
@@ -2,5 +2,13 @@
 /bin/bash ./backup_script.sh $1 $2
 
 
-echo "Listening..."
-iwatch -r -c "/bin/bash ./backup_script.sh $1 $2" $1 
+#pega o caminho absoluto da pasta de origem passada como parametro, pois o auditctl não aceita caminhos relativos
+absolute_path=$(readlink -f $1)
+
+# cria a regra de audit para moniorar os "writes" da pasta, passando como parametro no -w o caminho absoluto da pasta, no -p o tipo de evento que ele vai escutar (no caso apenas o de write) e uma chave no -k (utilizada para localizar o evento no log)
+auditctl -
+auditctl -w $absolute_path -p w -k pasta
+
+# inicia o iwatch
+echo "Listening..." #printa listening
+iwatch -r -c "sudo /bin/bash ./backup_script.sh $1 $2" $1 #inicia o iwatch de forma recursiva e executa o script de backup no parametro -c
